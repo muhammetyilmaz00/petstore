b2fc51efc97d5f04bd81ddd447ccdea4de99ce04
muhammetyilmaz00 <muhammetyilmaz00@github.com>
2021-12-12 22:39:14 +0300

    Hooks and Log4j added

---
+++ src/test/java/petStore/utils/LogUtils.java
@@ -0,0 +1,13 @@
+package petStore.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogUtils.class);
+
+    public static void logInfo(String text){
+        logger.info(text);
+    }
+}
